34f15bab615ee63bb4c9f4d32744742e77338e86
Saba Mustafa <sabaali3738@gmail.com>
2023-10-31 23:58:08 +0500

    Calculator

---
+++ Calculator/index.js
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        type: "number",
+        name: "numberOne",
+        message: "kindly enter your first no:",
+    },
+    {
+        type: "number",
+        name: "numbertwo",
+        message: "kindly enter your second no:",
+    },
+    {
+        type: "list",
+        name: "operator",
+        choices: ["*", "+", "-", "/"],
+        message: "Select Operator: "
+    },
+]);
+const { numberone, numbertwo, operator } = answers;
+if (numberone && numbertwo && operator) {
+    let result = 0;
+    if (operator === "+") {
+        result = numberone + numbertwo;
+    }
+    else if (operator === "-") {
+        result = numberone - numbertwo;
+    }
+    if (operator === "/") {
+        result = numberone / numbertwo;
+    }
+    if (operator === "*") {
+        result = numberone * numbertwo;
+    }
+    console.log("Your result is : ,result");
+}
+else {
+    console.log("kindly invalid input ");
+}
